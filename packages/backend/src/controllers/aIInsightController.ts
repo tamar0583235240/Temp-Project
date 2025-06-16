250a39f2007dee36cd3d2db66ba7ae4582d127d1
tehila <t0527135298@gmail.com>
2025-06-16 20:18:49 +0300

    before pull

--- packages/backend/src/controllers/aIInsightController.ts
+++ packages/backend/src/controllers/aIInsightController.ts
@@ -1,1 +1,13 @@
 import { Request, Response } from 'express';
+import allnsigthRepository from '../reposioty/allnsigthRepository';
+
+export const getAllnsigth = async (req: Request, res: Response): Promise<void> => {
+//   console.log('exampleController called');
+  try {
+    const items = await allnsigthRepository.getAllallnsight();
+    res.json(items);
+  } catch (error) {
+    console.error('Error in exampleController:', error);
+    res.status(500).json({ error });
+  }
+};
