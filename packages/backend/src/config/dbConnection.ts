9bd1e9dd9f3130c7b8fe04a59681eee95a7551a5
TamiMe-dev <tamimedan2880@gmail.com>
2025-06-17 01:30:42 +0300

    initial project in the branch and arrange the conect to db

--- packages/backend/src/config/dbConnection.ts
+++ packages/backend/src/config/dbConnection.ts
@@ -1,14 +1,14 @@
 import dotenv from 'dotenv';
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SERVICE_ROLE_KEY!;
+// const supabaseUrl = process.env.SUPABASE_URL!;
+// const supabaseKey = process.env.SERVICE_ROLE_KEY!;
 
-console.log('SUPABASE_KEY: i am here', supabaseKey);
+// console.log('SUPABASE_KEY: i am here', supabaseKey);
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// export const supabase = createClient(supabaseUrl, supabaseKey);
 // src/config/pgClient.ts
 import { Pool } from 'pg';
 
--- packages/backend/src/reposioty/exampleReposioty.ts
+++ packages/backend/src/reposioty/exampleReposioty.ts
@@ -1,22 +1,22 @@
 
-import {supabase} from '../config/dbConnection';
-import { exampleInterface } from "../interfaces/exampleInterface";
+// // import {supabase} from '../config/dbConnection';
+// import { exampleInterface } from "../interfaces/exampleInterface";
 
-const getAllExamples = async (): Promise<exampleInterface[]> => {
-  try {
-    const { data, error } = await supabase
-      .from("examples")
-      .select("exampleField1, exampleField2, exampleField3");
+// const getAllExamples = async (): Promise<exampleInterface[]> => {
+//   try {
+//     const { data, error } = await supabase
+//       .from("examples")
+//       .select("exampleField1, exampleField2, exampleField3");
 
-    if (error) {
-      throw error;
-    }
+//     if (error) {
+//       throw error;
+//     }
 
-    return data as exampleInterface[];
-  } catch (error) {
-    console.error("Error fetching examples from Supabase:", error);
-    throw error;
-  }
-};
+//     return data as exampleInterface[];
+//   } catch (error) {
+//     console.error("Error fetching examples from Supabase:", error);
+//     throw error;
+//   }
+// };
 
-export default { getAllExamples };
+// export default { getAllExamples };
