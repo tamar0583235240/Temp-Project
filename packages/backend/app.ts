218161b6a30b638f8dc07482fdaa7bb020c9d726
ShaniWeill <sh0548572701@gmail.com>
2025-06-17 14:44:18 +0300

    ai insights

--- packages/backend/app.ts
+++ packages/backend/app.ts
@@ -1,33 +1,13 @@
-// import exampleRouts from './src/routes/exampleRouts'
-// import dotenv from 'dotenv';
-// import express from 'express';
-// import cors from 'cors';
-
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 5001;
-
-// app.use(express.json());
-// // example for implemantaion
-// app.use('/api', exampleRouts)
-
-// app.use(cors());
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// })
-
 import express, { Application } from 'express';
 import cors from 'cors';
 import exampleRouts from './src/routes/exampleRouts';
 import aIInsightRouts from './src/routes/aIInsightRouts';
-// import {client} from './src/config/dbConnection';
 
-const app: Application = express();
+const app = express();
+
 console.log('i am here in app');
-app.use(express.json());
 app.use(cors());
-
+app.use(express.json());
 app.use('/api', exampleRouts);
 app.use('/api/insights', aIInsightRouts);
 
