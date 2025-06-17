85d283ec23eee1ec5aedd583ef3f116d7a205f98
TamiMe-dev <tamimedan2880@gmail.com>
2025-06-17 15:37:10 +0300

    add TypeORM entities and configs

---
+++ packages/backend/src/interfaces/entities/Resources.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, Index } from "typeorm";
+
+@Index("resources_pkey", ["id"], { unique: true })
+@Entity("resources", { schema: "public" })
+export class Resources {
+  @Column("uuid", { primary: true, name: "id" })
+  id: string;
+
+  @Column("text", { name: "title" })
+  title: string;
+
+  @Column("text", { name: "type" })
+  type: string;
+
+  @Column("text", { name: "description" })
+  description: string;
+
+  @Column("text", { name: "file_url" })
+  fileUrl: string;
+
+  @Column("timestamp without time zone", {
+    name: "created_at",
+    default: () => "now()",
+  })
+  createdAt: Date;
+}
