408880d19f4e3d509ecb5170586c9ecbcc1f6a32
tehila46247 <t0527146247@gmail.com>
2025-06-17 15:27:27 +0300

    backend-getAiInsights

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { FileUpload } from './shared/components/FileUpload';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1>Project base viewer</h1>
       </header>
+      <FileUpload />
       <main>
       </main>
     </div>
