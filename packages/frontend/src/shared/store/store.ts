1910f8bd788517242dd1977337709031ce10f333
Reisy <reisy1b@gmail.com>
2025-06-18 12:59:54 +0300

    simulationSlice

--- packages/frontend/src/shared/store/store.ts
+++ packages/frontend/src/shared/store/store.ts
@@ -2,10 +2,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "../api/api";
 import exampleSlice from '../../features/exampleFeatures/store/exampleSlice'
+import simulationSlice from '../../features/interview/store/slices/simulationSlice';
+
+
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     example: exampleSlice,
+    simulation: simulationSlice, 
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
