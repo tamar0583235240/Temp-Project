c1481bdb2e588a5ae3051f98563c4b7955e24789
rivka-0548582569 <rivkahorowitz2005@gmail.com>
2025-06-19 17:33:16 +0300

    adding backend-thersday afternoon

--- packages/backend/src/routes/authRouts.ts
+++ packages/backend/src/routes/authRouts.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { login, signup } from '../controllers/authController';
+import { generateAndSendCode, login, signup, validateCode } from '../controllers/authController';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/signup', signup);
+//יצירת הקוד ושליחתו במייל
+router.post('/createValidationCode',  generateAndSendCode);
+//בדיקה אם הקוד תקין
+router.post('/validateCode',  validateCode);
 
 export default router;
