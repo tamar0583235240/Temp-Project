7061ab86b393dbf40f31ab065a33c576a43bb51e
rivkishenberger <r0548556409@gmail.com>
2025-06-19 11:15:32 +0300

    add  Api frontend

--- packages/frontend/src/shared/api/userApi.ts
+++ packages/frontend/src/shared/api/userApi.ts
@@ -9,8 +9,33 @@ export const userApi = api.injectEndpoints({
     getUserById: builder.query<User, string>({
       query: (id) => `/users/${id}`,
     }),
+     addUser: builder.mutation<User, Partial<User>>({
+      query: (newUser) => ({
+        url: '/users',
+        method: 'POST',
+        body: newUser,
+      }),
+    }),
+    updateUser: builder.mutation<User, { id: string; data: Partial<User> }>({
+      query: ({ id, data }) => ({
+        url: `/users/${id}`,
+        method: 'PUT',
+        body: data,
+      }),
+    }),
+    deleteUser: builder.mutation<{ success: boolean }, string>({
+      query: (id) => ({
+        url: `/users/${id}`,
+        method: 'DELETE',
+      }),
+    }),
   }),
 });
 
-export const { useGetUsersQuery, useGetUserByIdQuery } = userApi;
-
+export const { 
+  useGetUsersQuery, 
+  useGetUserByIdQuery, 
+  useAddUserMutation, 
+  useUpdateUserMutation, 
+  useDeleteUserMutation 
+} = userApi;
