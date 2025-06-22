ebeee1a9051eb1d264f1d0081e581c413d804cbe
tehila <t0527135298@gmail.com>
2025-06-22 14:36:21 +0300

    before marge

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,28 +1,7 @@
-// import React from 'react';
-// import './App.css';
-// import UserList from './features/admin/UserList';
-// import ProgressStats from './features/dashboard/components/ProgressStats'
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Project base viewer</h1>
-//       </header>
-//       <main>
-//                 <UserList />
-//                 <ProgressStats/>
-
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import './App.css';
-import UserList from './features/admin/UserList';
 import ProgressStats from './features/dashboard/components/ProgressStats';
+import UserList from './features/admin/UserList';
 
 function App() {
   return (
