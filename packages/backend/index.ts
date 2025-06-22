540d21329d8b81d79f3bf64e82c09c325e7dfd65
Reisy <reisy1b@gmail.com>
2025-06-22 11:53:37 +0300

    3001 to 5000

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import app from './app';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
