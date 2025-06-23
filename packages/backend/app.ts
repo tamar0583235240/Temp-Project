975aa73ce9083cf1ef6d07a4d7205bb1dffe384e
TamiMe-dev <tamimedan2880@gmail.com>
2025-06-23 10:18:02 +0300

    add Interview Preparation Hub Infrastructurt

--- packages/backend/app.ts
+++ packages/backend/app.ts
@@ -9,6 +9,8 @@ const app: Application = express();
 console.log('i am here in app');
 app.use(express.json());
 app.use('/api', exampleRouts);
+app.use('/api', exampleRouts);
+
 app.use(cors());
 
 export default app;
