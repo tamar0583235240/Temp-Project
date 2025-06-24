c73f09902e41d607d91ce5d7ab8788409586db1e
BatShevagit <sh0548503408@gmail.com>
2025-06-24 10:03:24 +0300

    save changes in utils

--- packages/frontend/src/shared/utils/cn.ts
+++ packages/frontend/src/shared/utils/cn.ts
@@ -1,7 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// מחבר classNames ומוודא שאין התנגשויות ב־Tailwind
 export function cn(...inputs: any[]) {
   return twMerge(clsx(...inputs));
 }
