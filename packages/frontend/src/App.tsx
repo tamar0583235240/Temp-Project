5dcb2b1d2b0711f9ad0c3c3452586a59a3b0f34a
kastelanitz <skas24680@gmail.com>
2025-06-24 10:25:43 +0300

    ok

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './shared/routes/appRoutes'
 import './App.css';
+import { AddQuestion } from './features/admin/components/addQuestion';
 
 function App() {
   return (
       <BrowserRouter>
       <AppRoutes />
+      <AddQuestion></AddQuestion>
     </BrowserRouter>
   );
 }
--- packages/frontend/src/features/admin/components/addQuestion.tsx
+++ packages/frontend/src/features/admin/components/addQuestion.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react"
+import { useAddQuestionMutation } from "../services/adminQuestionApi"
+
 export const AddQuestion = () => {
+    const [addQuestion] = useAddQuestionMutation()  
+    const [newQuestion , serNewQuestion] = useState({
+        title: "",
+        content: "",
+        category: "",
+        tips: "",
+        aiGuidance: "",
+        isActive: true
+    })
+
     return <div>
         <button>הוספת שאלה</button>
+        <dialog>
+        <form>
+            <h3>הוספת שאלה למאגר השאלות</h3><br></br>
+            <label>שם השאלה</label>
+            <input required type="text" placeholder="שם השאלה" />
+            <label>תוכן השאלה</label>
+            <input required type="text" placeholder="תוכן השאלה" />
+            <label>קטגוריה</label>
+            <input type="text" placeholder="קטגוריה" />
+            <label>טיפים למענה</label>
+            <input type="text" placeholder="טיפים" />
+            <label>הוראות AIל</label>
+            <input type="text" placeholder="הוראות AIל" />
+        </form>
+        </dialog>
     </div>
 }
