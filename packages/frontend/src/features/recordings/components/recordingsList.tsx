4b1830004fc9a0e0eb89f1e698551abe4e1bb021
kastelanitz <skas24680@gmail.com>
2025-06-25 11:27:21 +0300

    ok is before merge pull with domb

--- packages/frontend/src/features/recordings/components/recordingsList.tsx
+++ packages/frontend/src/features/recordings/components/recordingsList.tsx
@@ -16,7 +16,7 @@ type RecordingsListProps = {
 export const RecordingsList: React.FC<RecordingsListProps> = ({ allowedRoles }) => {
     const user = useSelector((state: RootState) => state.auth.user);
     // שורה זו צריך לשנות לאחר שיש את הנתונים של המשתמש הנוכחי שנמצא כעת באתר
-    const userId = user && user.id ? user.id.toString() : '550e8400-e29b-41d4-a718-446655440000';
+    const userId = user && user.id ? user.id.toString() : '00000000-0000-0000-0000-000000000000';
     const { data, error, isLoading } = useGetAnswersByIdUserQuery(userId);
 
     if (isLoading)
--- packages/frontend/src/shared/api/api.ts
+++ packages/frontend/src/shared/api/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5001/",
+        baseUrl: "http://localhost:5000/",
 
     }),
     reducerPath: "api",
