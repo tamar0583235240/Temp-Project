46b08b65f3d489a179e7094cc7c5af645fdbdd5d
ShaniWeill <sh0548572701@gmail.com>
2025-06-25 13:02:02 +0300

    add try & catch

--- packages/backend/src/controllers/reminderController.ts
+++ packages/backend/src/controllers/reminderController.ts
@@ -6,8 +6,19 @@ export const reminderController = async (req: Request, res: Response): Promise<v
   try {
     const items = await reminderRepository.getAllTips();
     res.json(items);
-  } catch (error) {
+  } catch (error: any) {
     console.error('Error in exampleController:', error);
-    res.status(500).json({ error });
+
+    if (error?.type === 'redirect') {
+      res.status(300).json({ message: 'Redirect required', location: '/other-page' });
+    }
+
+    else if (error?.type === 'bad_request') {
+      res.status(400).json({ error: 'Bad Request' });
+    }
+
+    else {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 };
