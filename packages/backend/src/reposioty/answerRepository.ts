f6f488861a4a0587662456032a005685497293ba
tamar0583235240 <tamar0583235240@gmail.com>
2025-06-26 09:53:35 +0300

    merge to  group4

--- packages/backend/src/reposioty/answerRepository.ts
+++ packages/backend/src/reposioty/answerRepository.ts
@@ -4,12 +4,7 @@ import { Answers } from "../interfaces/entities/Answers";
 const getAllAnswersByIdUser = async (userId:string): Promise<Answers[]> => {
   
   try{
-<<<<<<< HEAD
-    // const query = 'SELECT id, user_id, question_id, file_url,answer_file_name ,submitted_at FROM answers WHERE user_id = \$1';
     const query = 'SELECT id, user_id, question_id, file_url,answer_file_name ,submitted_at FROM answers WHERE user_id = $1';
-=======
-    const query = 'SELECT id, user_id, question_id, file_url,answer_file_name ,submitted_at,amount_feedbacks FROM answers WHERE user_id = \$1';
->>>>>>> 8d7d8a21a7e51e0e565bee46575e3ba71e17593d
     const values = [userId];
     const { rows } = await pool.query(query, values);
     //שורה זו - 12 - נכתבה כדי להציג הקלטות שנשמרו מקומית במחשב
