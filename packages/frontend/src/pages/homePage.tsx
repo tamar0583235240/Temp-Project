4ec2a576f7fcef7a8bd28d84e4639b24d4b173a7
ruchamy <r0527187861@gmail.com>
2025-06-29 15:53:22 +0300

    feat: Implement login form with Google authentication
    - Added GoogleAuthButton component for Google login functionality.
    - Created LoginForm component to handle user login with email and password.
    - Integrated GoogleAuthButton into LoginForm.
    - Updated App component to render LoginForm.
    - Added user and authentication-related types in types.ts.
    - Updated package.json to include new dependencies for authentication and UI components.
    - Cleaned up homePage component by removing commented-out navigation buttons.

--- packages/frontend/src/pages/homePage.tsx
+++ packages/frontend/src/pages/homePage.tsx
@@ -17,40 +17,7 @@ const HomePage = () => {
 
 
   return (
-    // <GridContainer maxWidth="md" gridClasses="grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-4" mt="mt-4" mb="mb-4">
-    //   <Button variant="primary-dark" fullWidth onClick={() => navigate("/simulation")}>
-    //     התחלי סימולציה
-    //   </Button>
-    //   <Button variant="primary-dark" fullWidth onClick={() => navigate("/dashboard")}>
-    //     דשבורד
-    //   </Button>
-    //   <Button variant="primary-dark" fullWidth onClick={() => navigate("/resources")}>
-    //     משאבים
-    //   </Button>
-    //   <Button variant="primary-dark" fullWidth onClick={() => navigate("/recordings")}>
-    //     הקלטות
-    //   </Button>
-    //   <Button variant="primary-dark" fullWidth onClick={() => navigate("/shared")}>
-    //     משותף
-    //   </Button>
-
-    //   {isManager && (
-    //     <>
-    //     <SidebarNavigation />
-    //       <div className="col-span-full mt-6 text-right">
-    //         <h2 className="text-xl font-bold text-text-main">מנהל מערכת</h2>
-    //       </div>
-    //       <Button variant="outline" fullWidth onClick={() => navigate("/admin/questions")}>
-    //         ניהול שאלות
-    //       </Button>
-    //       <Button variant="outline" fullWidth onClick={() => navigate("/admin/resources")}>
-    //         ניהול משאבים
-    //       </Button>
-    //       <Button variant="outline" fullWidth onClick={() => navigate("/admin/users")}>
-    //         ניהול משתמשים
-    //       </Button>
-    //     </>
-    //   )}
+   
     // </GridContainer>
     <div className="flex h-screen">
       <SidebarNavigation />
