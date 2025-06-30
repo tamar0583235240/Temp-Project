25ee24dc16654459e7c3c79bac28520f8d71b37f
ruchamy <r0527187861@gmail.com>
2025-06-30 19:30:50 +0300

    feat: Update routing and sidebar navigation for admin roles

--- packages/frontend/src/shared/routes/appRoutes.tsx
+++ packages/frontend/src/shared/routes/appRoutes.tsx
@@ -6,96 +6,98 @@ import LoginForm from "../../features/auth/components/LoginForm";
 import SignupForm from "../../features/auth/components/SignupForm";
 import DashboardLayout from "../ui/DashboardLayout";
 import ResetPassword from "../../features/auth/components/ResetPassword";
-import { CreateInterviewMaterialsSubForm } from "../../features/knowledge-base/components/CreateInterviewMaterialsSubForm";
-import InterviewMaterialPage from "../../features/knowledge-base/components/InterviewMaterialPage";
+import LandingPage from "../../pages/LandingPage";
 
 export default function AppRoutes() {
   return (
-  <div dir="rtl">
-    <Routes>
-      {/* Routes without sidebar */}
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/signup" element={<SignupForm />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-      {/* Routes with sidebar */}
-      <Route element={<DashboardLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route
-          path="/simulation"
-          element={
-            <RoleProtectedRoute allowedRoles={['student']}>
-              <p>Simulation</p>
-            </RoleProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <RoleProtectedRoute allowedRoles={['student']}>
-              <p>Dashboard</p>
-            </RoleProtectedRoute>
-          }
-        />
-        <Route
-          path="/recordings"
-          element={
-            <RoleProtectedRoute allowedRoles={['student']}>
-              <p>Recordings</p>
-            </RoleProtectedRoute>
-          }
-        />
-        <Route
-          path="/shared"
-          element={
-            <RoleProtectedRoute allowedRoles={['student']}>
-              <p>SharedRecordings</p>
-            </RoleProtectedRoute>
-          }
-        />
-        <Route
-          path="/resources"
-          element={
-            <RoleProtectedRoute allowedRoles={['student']}>
-              <p>Resources</p>
-            </RoleProtectedRoute>
-          }
-        />
-        <Route
-          path="/interviewMaterialsHub"
-          element={
-            <RoleProtectedRoute allowedRoles={['student']}>
-              <p>InterviewMaterialsHub</p>
-            </RoleProtectedRoute>
-          }
-        />
-        <Route
-          path="/manager/questions"
-          element={
-            <RoleProtectedRoute allowedRoles={["manager"]}>
-              <p>AdminQuestions</p>
-            </RoleProtectedRoute>
-          }
-        />
-        <Route
-          path="/manager/users"
-          element={
-            <RoleProtectedRoute allowedRoles={["manager"]}>
-              <p>AdminUsers</p>
-            </RoleProtectedRoute>
-          }
-        />
-        <Route
-          path="/manager/resources"
-          element={
-            <RoleProtectedRoute allowedRoles={["manager"]}>
-              {/* <p>AdminResources</p> */}
-              <InterviewMaterialPage />
-            </RoleProtectedRoute>
-          }
-        />
-      </Route>
-    </Routes>
-  </div>
+    <div dir="rtl">
+      <Routes>
+        {/* Routes without sidebar */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={
+          <RoleProtectedRoute allowedRoles={['student', 'manager']}>
+            <ResetPassword />
+          </RoleProtectedRoute>} />
+        {/* Routes with header */}
+        <Route path="/home" element={<HomePage />} />
+        <Route element={<DashboardLayout />}>
+          <Route
+            path="/simulation"
+            element={
+              <RoleProtectedRoute allowedRoles={['student', 'manager']}>
+                <p>Simulation</p>
+              </RoleProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <RoleProtectedRoute allowedRoles={['student', 'manager']}>
+                <p>Dashboard</p>
+              </RoleProtectedRoute>
+            }
+          />
+          <Route
+            path="/recordings"
+            element={
+              <RoleProtectedRoute allowedRoles={['student', 'manager']}>
+                <p>Recordings</p>
+              </RoleProtectedRoute>
+            }
+          />
+          <Route
+            path="/shared"
+            element={
+              <RoleProtectedRoute allowedRoles={['student', 'manager']}>
+                <p>SharedRecordings</p>
+              </RoleProtectedRoute>
+            }
+          />
+          <Route
+            path="/resources"
+            element={
+              <RoleProtectedRoute allowedRoles={['student', 'manager']}>
+                <p>Resources</p>
+              </RoleProtectedRoute>
+            }
+          />
+          <Route
+            path="/interviewMaterialsHub"
+            element={
+              <RoleProtectedRoute allowedRoles={['student', 'manager']}>
+                <p>InterviewMaterialsHub</p>
+              </RoleProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/questions"
+            element={
+              <RoleProtectedRoute allowedRoles={["manager"]}>
+                <p>AdminQuestions</p>
+              </RoleProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/users"
+            element={
+              <RoleProtectedRoute allowedRoles={["manager"]}>
+                <p>AdminUsers</p>
+              </RoleProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/resources"
+            element={
+              <RoleProtectedRoute allowedRoles={["manager"]}>
+                <p>AdminResources</p>
+              </RoleProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </div>
   );
 }
