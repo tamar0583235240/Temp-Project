2bc6984e1234d9df4ae6752459ed8a1a3cd3ee39
rutiKremer <ruti104667@gmail.com>
2025-06-30 16:04:45 +0300

    finally
    after all

--- packages/frontend/src/features/recordings/components/recordingsList.tsx
+++ packages/frontend/src/features/recordings/components/recordingsList.tsx
@@ -18,7 +18,7 @@ import { useGetAiInsightsQuery } from "../services/AiInsightsApi";
 export const RecordingsList: React.FC<{ allowedRoles: string[] }> = ({ allowedRoles }) => {
     const user = useSelector((state: RootState) => state.auth.user);
     // שורה זו צריך לשנות לאחר שיש את הנתונים של המשתמש הנוכחי שנמצא כעת באתר
-    const userId = user?.id ?? '550e8400-e29b-41d4-a718-446655440000';
+    const userId = user?.id ?? 'ef412672-cd67-41fb-a679-6d8b916e25a8';
     const { data, error, isLoading } = useGetAnswersByIdUserQuery(userId);
 
     const { data: allInsights, error: aiError, isLoading: isAiLoading } = useGetAiInsightsQuery();
