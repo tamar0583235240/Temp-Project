869e6b1c91b199d3b3d7b3860b3e19867b622cd6
rivka-0548582569 <rivkahorowitz2005@gmail.com>
2025-07-01 10:19:04 +0300

    front end without api

--- packages/browser-extension/src/hooks/useMockProgress.ts
+++
@@ -1,7 +0,0 @@
-import type { ProgressData } from '../components/ProgressStats';
-
-export const useMockProgress = (): ProgressData | null => {
-
-const data:ProgressData= {total:40,completed:22};
-  return data;
-};
