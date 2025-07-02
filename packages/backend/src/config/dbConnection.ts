570a61e9e9f43151e1e887d30cb1f1a638c958e7
Rachel33161 <diskind.rachel@gmail.com>
2025-07-02 13:50:04 +0300

    fix_dbConnection

--- packages/backend/src/config/dbConnection.ts
+++ packages/backend/src/config/dbConnection.ts
@@ -2,13 +2,12 @@ import { Pool } from 'pg';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-console.log('Database user:', process.env.DB_USER);
-console.log('Database host:', process.env.DB_HOST);
-console.log('Database name:', process.env.DB_NAME);
-console.log('Database password:', process.env.DB_PASSWORD);
-console.log('Type:', typeof process.env.DB_PASSWORD);
+console.log('Database user:', process.env.PGUSER);
+console.log('Database host:', process.env.PGHOST);
+console.log('Database name:', process.env.PGDATABASE);
+console.log('Database password:', process.env.PGPASSWORD);
+console.log('Database port:', process.env.PGPORT);
 
-console.log('Database port:', process.env.DB_PORT);
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
