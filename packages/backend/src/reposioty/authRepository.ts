2cf9cfa2a6fa0f2077fe10687424b03b628ad387
ruchamy <r0527187861@gmail.com>
2025-07-02 10:28:28 +0300

    Add password field to user signup query

--- packages/backend/src/reposioty/authRepository.ts
+++ packages/backend/src/reposioty/authRepository.ts
@@ -18,8 +18,8 @@ const signup = async (userData: Users): Promise<Users> => {
     const { id, first_name, last_name, email, phone, role, createdAt, isActive, password} = userData;
 
     const res = await pool.query(
-      `INSERT INTO users (id, first_name, last_name, email, phone, role, created_at, is_active)
-       VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      `INSERT INTO users (id, first_name, last_name, email, phone, role, created_at, is_active, password)
+       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
        RETURNING *`,
       [id, first_name, last_name, email, phone, role, createdAt, isActive, password]
     );
