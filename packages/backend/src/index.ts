d5a240c0fba4657c9e34a7721d22eaf6e7fac14f
Rachel33161 <diskind.rachel@gmail.com>
2025-07-03 15:02:27 +0300

    with out warnings to git

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,19 +1,19 @@
 
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 
-console.log('SUPABASE_URL:', process.env.SUPABASE_URL,'index');
-console.log("sdfghjuikolp0")
-console.log('SUPABASE_SERVICE_ROLE_KEY:', process.env.SERVICE_ROLE_KEY,'index 2222222');
+// console.log('SUPABASE_URL:', process.env.SUPABASE_URL,'index');
+// console.log("sdfghjuikolp0")
+// console.log('SUPABASE_SERVICE_ROLE_KEY:', process.env.SERVICE_ROLE_KEY,'index 2222222');
 
-import app from './app';
+// import app from './app';
 
-const PORT = process.env.PORT || 5001;
+// const PORT = process.env.PORT || 5001;
 
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
