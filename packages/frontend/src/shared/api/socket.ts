77101107574e03266ea200a77feab20494623f0c
kastelanitz <skas24680@gmail.com>
2025-07-03 15:43:16 +0300

    ok the socket client and serve is working

--- packages/frontend/src/shared/api/socket.ts
+++ packages/frontend/src/shared/api/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { store } from '../store/store'; // ייבוא ה-store שלך
 import { AdminQuestionApi } from '../../features/admin/services/adminQuestionApi'; // ייבוא ה-RTK Query שלך
-import { log } from "console";
+import { userApi } from "./userApi";
 
 const socket = io("http://localhost:5002"); // שנה לכתובת השרת שלך
 
@@ -9,11 +9,20 @@ socket.on("connect", () => {
     console.log("Connected to server");
 });
 
-socket.on("questionDeleted", (newData) => {
+socket.on("questionDeleted", (newQuestionsList) => {
     // עדכון ה-cache ב-Redux
-    console.log("I updated the cache");
-    
-    store.dispatch(AdminQuestionApi.util.updateQueryData('getAllQuestions', undefined, () => newData));
+    console.log("I updated the questions cache");
+    store.dispatch(AdminQuestionApi.util.updateQueryData('getAllQuestions', undefined, () => newQuestionsList));
+});
+
+socket.on("interviewMaterialSubUpdated", (newInterviewMaterialSubList) => {
+    console.log("I updated the interviewMaterialSub cache");
+    // store.dispatch(interviewMaterialSubApi.util.updateQueryData('getInterviewMaterialSub', undefined, () => newInterviewMaterialSubList));
+});
+
+socket.on("userAdded", (newUsersList) => {
+    console.log("I updated the users cache");
+    store.dispatch(userApi.util.updateQueryData('getUsers', undefined, () => newUsersList));
 });
 
 export default socket;
