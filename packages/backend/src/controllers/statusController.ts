569e380aa7467f7dd14910afce60b2d971488f55
Reisy <reisy1b@gmail.com>
2025-07-06 15:44:12 +0300

    יחיח

--- packages/backend/src/controllers/statusController.ts
+++ packages/backend/src/controllers/statusController.ts
@@ -18,4 +18,15 @@ export const getUserAnsweredQuestionsControler = async (req: Request, res: Respo
     throw error;
   }
 };
-export default { getStatusByUserId , saveOrUpdateStatus };
+// Define or import these functions before exporting
+const getStatusByUserId = async (req: Request, res: Response) => {
+  // TODO: Implement this function
+  res.status(501).json({ message: 'Not implemented' });
+};
+
+const saveOrUpdateStatus = async (req: Request, res: Response) => {
+  // TODO: Implement this function
+  res.status(501).json({ message: 'Not implemented' });
+};
+
+export default { getStatusByUserId, saveOrUpdateStatus };
