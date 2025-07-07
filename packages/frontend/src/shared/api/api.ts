72f9ba4d65614382f273d45bd4ed24f79a9c6910
tamar0583235240 <tamar0583235240@gmail.com>
2025-07-07 10:41:22 +0300

    CI test

--- packages/frontend/src/shared/api/api.ts
+++ packages/frontend/src/shared/api/api.ts
@@ -1,5 +1,5 @@
 // api.ts
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
