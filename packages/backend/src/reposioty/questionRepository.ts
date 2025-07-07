f551ffa7e5deddeafbf5389431e968b7ef21e34e
ROren7878 <roren@example.com>
2025-07-07 11:36:09 +0300

    run

--- packages/backend/src/reposioty/questionRepository.ts
+++ packages/backend/src/reposioty/questionRepository.ts
@@ -1,37 +1,37 @@
 import { pool } from '../config/dbConnection';
 import { Question } from '../interfaces/entities/Question';
-
 const getAllQuestions = async (): Promise<Question[]> => {
   try {
     const query = `
-      SELECT id, title, content, category, tips, ai_guidance, is_active, options, question_type
+      SELECT id, title, content, category, tips, ai_guidance, is_active
       FROM "questions"
       WHERE is_active = TRUE
     `;
-    // const query = `
-    //   SELECT * FROM "questions"
-    // `;
     const result = await pool.query(query);
     return result.rows as Question[];
   } catch (error) {
-    console.error("❌ Error fetching questions:", error);
+    console.error(":x: Error fetching questions:", error);
     throw error;
   }
 };
-
 const getQuestionsByCategory = async (category_id: string): Promise<Question[]> => {
   try {
    const query = `
-      SELECT id, title, content, category, tips, ai_guidance, is_active, options, question_type
+      SELECT id, title, content, category, tips, ai_guidance, is_active
       FROM "questions"
       WHERE category_id = $1
     `;
     const result = await pool.query(query, [category_id]);
     return result.rows as Question[];
   } catch (error) {
-    console.error("❌ Error fetching questions by category:", error);
+    console.error(":x: Error fetching questions by category:", error);
     throw error;
   }
 };
-
 export default { getAllQuestions, getQuestionsByCategory };
+
+
+
+
+
+
--- packages/frontend/src/pages/InterviewPage.tsx
+++ packages/frontend/src/pages/InterviewPage.tsx
@@ -10,7 +10,7 @@ import EndSurvey from "../features/interview/components/endSurvey";
 import ShowCategories from "../features/interview/components/showCategories";
 
 const InterviewPage = () => {
-  const categoryId = "77777777-7777-7777-7777-777777777777"
+  const categoryId = "55555555-5555-5555-5555-555555555555"
   const navigate = useNavigate();
   const { data: questions = [], isLoading, isError } = useGetQuestionsByCategoryQuery(categoryId);
 
