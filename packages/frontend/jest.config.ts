2287c3ecdcd4b029a64b812155994a8f3efe18fd
tamar0583235240 <tamar0583235240@gmail.com>
2025-07-07 11:35:51 +0300

    CI test

--- packages/frontend/jest.config.ts
+++ packages/frontend/jest.config.ts
@@ -1,8 +1,17 @@
-module.exports = {
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'jsdom',
+//   transformIgnorePatterns: [
+//     "/node_modules/(?!@reduxjs|@standard-schema).+\\.js$"
+//   ],
+// };
+
+export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./src/setupTests.ts'],
   transformIgnorePatterns: [
-    "/node_modules/(?!@reduxjs|@standard-schema).+\\.js$"
+    '/node_modules/(?!(\\@standard-schema)/)',
   ],
 };
 
