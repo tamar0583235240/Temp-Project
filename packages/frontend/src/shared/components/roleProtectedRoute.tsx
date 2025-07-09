ccdee96338bdb3672cfc62ab1727a5ae0381426f
kastelanitz <skas24680@gmail.com>
2025-07-09 11:24:32 +0300

    llll

--- packages/frontend/src/shared/components/roleProtectedRoute.tsx
+++ packages/frontend/src/shared/components/roleProtectedRoute.tsx
@@ -1,19 +1,22 @@
+import { Navigate } from "react-router-dom";
 import type { JSX } from "react";
 import { RootState, store } from "../store/store";
 import { useSelector } from "react-redux";
 import { User } from "../../features/auth/types/types";
-import { Navigate } from "react-router-dom";
-
 interface Props {
   children: JSX.Element;
   allowedRoles: string[];
 }
-
 export function RoleProtectedRoute({ children, allowedRoles }: Props): JSX.Element {
   const user: User|null = useSelector((state: RootState) => state.auth.user);
   if (!user || !allowedRoles.includes(user.role)) {
     return <Navigate to="/login" replace />;
   }
   return children;
-  
 }
+
+
+
+
+
+
