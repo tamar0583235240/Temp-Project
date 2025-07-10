3991fb143f13196cba46b37b865ed6017db3a802
Sheni-develop <sheinyl054@gmail.com>
2025-07-10 15:25:10 +0300

    check

--- packages/backend/app.ts
+++ packages/backend/app.ts
@@ -3,21 +3,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
-dotenv.config();
-
-const app: Application = express();
-
-// הגדרות CORS
-const corsOptions = {
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(cookieParser());
-
-// ראוטים
 import feedbackRouter from './src/routes/feedbackRouts';
 import AiInsightsRouter from './src/routes/aIInsightRouts';
 import answerRouter from './src/routes/answerRouts';
@@ -29,7 +14,21 @@ import authRouts from './src/routes/authRouts';
 import usedynamicContentRouter from './src/routes/DynamicContentRoutes';
 import userAdminRouts from './src/routes/userAdminRouts';
 import popularQuestionsRoute from './src/routes/popularQuestionsRoute';
-import exampleRouts from './src/routes/exampleRouts'
+import exampleRouts from './src/routes/exampleRouts';
+
+dotenv.config();
+
+const app: Application = express();
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
 // רישום ראוטים
 app.use('/api/feedback', feedbackRouter);
 app.use('/api/ai-insights', AiInsightsRouter);
