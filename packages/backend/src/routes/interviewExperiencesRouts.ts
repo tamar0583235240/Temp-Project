582dcafdb8122e3e4956a1785ba32250802d9561
rutDomb <ruti1558@gmail.com>
2025-07-13 13:59:38 +0300

    finish add

--- packages/backend/src/routes/interviewExperiencesRouts.ts
+++ packages/backend/src/routes/interviewExperiencesRouts.ts
@@ -1,9 +1,9 @@
 
-import { getAllInterviewExperiences } from '../controllers/interviewExperiencesController';
+import { addInterviewExperiences, getAllInterviewExperiences } from '../controllers/interviewExperiencesController';
 import { Router } from 'express';
 
 const interviewExperiencesRouter = Router();
 
 interviewExperiencesRouter.get('/getAllInterviewExperiences', getAllInterviewExperiences);  
-
+interviewExperiencesRouter.post('/addInterviewExperiences', addInterviewExperiences);  
 export default interviewExperiencesRouter;
