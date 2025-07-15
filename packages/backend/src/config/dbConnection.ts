e2be3877b01f83b14b1b3047a3c4db01cb8e2566
TamiMe-dev <tamimedan2880@gmail.com>
2025-07-15 18:54:59 +0300

    fix mistake

--- packages/backend/src/config/dbConnection.ts
+++ packages/backend/src/config/dbConnection.ts
@@ -25,12 +25,29 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { createClient } from '@supabase/supabase-js';
 
+console.log('[DB INIT]', {
+  PGHOST: process.env.DB_HOST,
+  PGPORT: process.env.DB_PORT,
+  PGUSER: process.env.DB_USER,
+  PGPASSWORD: process.env.DB_PASSWORD,
+  PGDATABASE: process.env.DB_NAME,
+});
+
 
 export const pool = new Pool({
-  host: process.env.PGHOST,
-  port: Number(process.env.PGPORT),
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
- });
+  host: String(process.env.DB_HOST),
+  port: Number(process.env.DB_PORT),
+  user: String(process.env.DB_USER),
+  password: String(process.env.DB_PASSWORD), // ← הכי חשוב
+  database: String(process.env.DB_NAME),
+});
+
+
+// export const pool = new Pool({
+//   host: process.env.PGHOST,
+//   port: Number(process.env.PGPORT),
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   database: process.env.PGDATABASE,
+//  });
 console.log('Database connection pool created');
--- packages/frontend/src/features/feedback/types/UsersType.ts
+++ packages/frontend/src/features/feedback/types/UsersType.ts
@@ -1,26 +1,10 @@
 export type Users = {
-  
   id: string;
-
-
   first_name: string;
-
-  
-  last_name: string;
-
-  
+ last_name: string;
   email: string;
-
- 
   phone: string | null;
-
-  
   role: string;
-
-  
   createdAt: Date;
-
- 
   isActive: boolean;
-
 }
